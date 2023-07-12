c61260e7bb7b357bc7cab350731f26fb6ec0e0eb
Priyanka <shahpriyanka9971@gmail.com>
2023-07-13 02:47:50 +0530

    first commit

---
+++ loginform/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginForm from './components/loginform';
+
+function App() {
+  return (
+    <div className="page">
+     <LoginForm/>
+    </div>
+  );
+}
+
+export default App;
